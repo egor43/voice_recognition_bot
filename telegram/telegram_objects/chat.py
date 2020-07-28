02cb2fa2b7bc22f16188dab2b3ffb156c92efc5c
ev.myshko <ev.myshko@tensor.ru>
2020-07-28 17:45:40 +0300

    Исправлена ошибка в объекте чата

--- telegram/telegram_objects/chat.py
+++ telegram/telegram_objects/chat.py
@@ -18,13 +18,13 @@ class Chat:
         """
         self.id = data["id"]
         self.type = data["type"]
-        self.title = get("title")
-        self.username = get("username")
-        self.first_name = get("first_name")
-        self.last_name = get("last_name")
-        self.description = get("description")
-        self.invite_link = get("invite_link")
+        self.title = data.get("title")
+        self.username = data.get("username")
+        self.first_name = data.get("first_name")
+        self.last_name = data.get("last_name")
+        self.description = data.get("description")
+        self.invite_link = data.get("invite_link")
         self.pinned_message = message.Message(data["pinned_message"]) if "pinned_message" in data else None
-        self.slow_mode_delay = get("slow_mode_delay")
-        self.sticker_set_name = get("sticker_set_name")
-        self.can_set_sticker_set = get("can_set_sticker_set")
+        self.slow_mode_delay = data.get("slow_mode_delay")
+        self.sticker_set_name = data.get("sticker_set_name")
+        self.can_set_sticker_set = data.get("can_set_sticker_set")
